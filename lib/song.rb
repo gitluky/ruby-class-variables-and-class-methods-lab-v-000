1a50e230a43b64ec1cc4339be35bb54293b4452e
Jonathan Luk <jluk87@gmail.com>
2018-03-09 00:40:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,31 @@
 class Song
+  @@count = 0
+  @@artists = []
+  @@genres = {}
+  
   attr_accessor :name, :artist, :genre
+  
   def initialize(name, artist, genre)
     self.name = name
     self.artist = artist
     self.genre = genre
+
+    @@count+=1
+    
   end
+
+  def self.count
+    @@count
+  end
+
+  def self.artist_count
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  
+  
 end
